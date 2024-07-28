81aa43406e2e7dbe8d2414aadfda21c90ca5414d
Air_Tom <167111183+Air-Tom@users.noreply.github.com>
2024-07-28 22:34:02 +0300

    Удалены лишние файлы
    Привидение в порядок репозитария.

--- Itmo.Labs/WinForms/Lab1/Itmo.CSDesktopApp.WinFormsLab_1-5-2/MainForm.cs
+++ Itmo.Labs/WinForms/Lab1/Itmo.CSDesktopApp.WinFormsLab_1-5-2/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -23,15 +24,22 @@ namespace Itmo.CSDesktopApp.WinFormsLab_1_5_2
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
-            System.Drawing.Drawing2D.GraphicsPath myPath =
-                new System.Drawing.Drawing2D.GraphicsPath();
-            myPath.AddPolygon(new Point[] { new Point(0, 0),
-                new Point(400, this.Height),
-            new Point(this.Width, 150) });
-            Region myRegion = new Region(myPath);
-            this.Region = myRegion;
+            using (GraphicsPath myPath = new GraphicsPath())
+            {
+                myPath.AddLines(new[]
+                    {
+                new Point(0, Height / 2),
+                new Point(Width / 2, 0),
+                new Point(Width, Height / 2),
+                new Point(Width / 2, Height)
+            });
+                Region = new Region(myPath);
+            }
         }
-
+        private void CloseButtonClick(object sender, EventArgs e)
+        {
+            Close();
+        }
 
 
     }
